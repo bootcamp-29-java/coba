6b5709e67ac94fd56ab4d20c364850eb5e6f4605
hp <hp@DESKTOP-E6TQIK0>
2019-08-19 13:15:57 +0700

    Perulangan for

---
+++ src/coba/AffanSout.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coba;
+
+/**
+ *
+ * @author hp
+ */
+public class AffanSout {
+    public static int ulang (int n){
+        
+        for (int i=0; i<n; i++) {
+            System.out.println(i);
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println("perulangan");
+        ulang(5);
+        
+    }
+    
+}
